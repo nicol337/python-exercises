4c1b3971a97dba129c4f1048f8c628d875302e97
Nicole Lee <nicolelee@Nicoles-MacBook-Pro.local>
2015-04-24 15:56:08 +0400

    completed unorderedList.py

--- node_basics.py
+++ node_basics.py
@@ -15,6 +15,7 @@ def unorderedListTest():
 	print('B', ol.search('B'))
 
 def unorderedListRemoveTest():
+	print("remove")
 	ol = UnorderedList()
 	ol.add('A')
 	ol.add('B')
@@ -23,16 +24,97 @@ def unorderedListRemoveTest():
 	ol.add('E')
 	ol.add('F')
 	ol.add('G')
-	ol.remove('D')
-	print(ol.items)
+	ol.remove('A')
+	ol.remove('B')
+	printList(ol)
 
 
+def unorderedListAppendTest():
+	ol = UnorderedList()
+	print("append")
+	printList(ol)
+	ol = UnorderedList()
+	ol.append('B')
+	ol.append('C')
+	ol.append('B')
+	printList(ol)
+
+def unorderedListPopTest():
+	print("pop")
+	# ol = UnorderedList()
+	# ol.append(1)
+	# ol.append(2)
+	# ol.add(-1)
+	# while not ol.isEmpty():
+	# 	print(ol.pop())
+	# printList(ol)
+
+	ol = UnorderedList()
+	ol.append('A')
+	ol.append('B')
+	ol.append('C')
+	ol.append('D')
+	printList(ol)
+	print('remove C')
+	print(ol.pop(2))
+	printList(ol)
+	print('remove D')
+	print(ol.pop(2))
+
+	# print(ol.pop(2))
+	# print(ol.pop(0))
+
+	# printList(ol)
+	# print(ol.pop(0))
+	# printList(ol)
+	# print(ol.pop(0))
+	# printList(ol)
 	
+def insertTest():
+	ol = UnorderedList()
+	ol.insert(0, 'G')
+	printList(ol)
+	ol.append('A')
+	ol.append('B')
+	ol.append('C')
+	ol.append('E')
+	ol.insert(3, 'D')
+	ol.insert(0, 'F')
+	printList(ol)
+
+def indexTest():
+	ol = UnorderedList()
+	ol.insert(0, 'A')
+	ol.append('B')
+	ol.append('C')
+	ol.append('D')
+	ol.append('E')
+	ol.append('F')
+	ol.append('G')
+
+	print(ol.index('G'))
+	print(ol.index('A'))
+	print(ol.index('I'))
+
+def printList(ol):
+	if not ol.head:
+		print("list is empty")
+	cur = ol.head
+	while cur != None:
+		print(cur.getData())
+		cur = cur.next
+
+
 
 def main():
-	nodeTest()
-	unorderedListTest()
-	unorderedListRemoveTest()
+	# nodeTest()
+	# unorderedListTest()
+	# unorderedListRemoveTest()
+	# unorderedListAppendTest()
+	# unorderedListPopTest()
+	# insertTest()
+	indexTest()
+
 
 
 if __name__ == "__main__":
